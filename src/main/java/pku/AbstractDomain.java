69087a72a1d7d75ce1bf7d6a4cc2365b52bb266b
yhtq <1414672068@qq.com>
2024-11-26 19:27:56 +0800

    Reapply "modify abstract domain"
    This reverts commit d65e549838feea4d5e46e4c0fd21af336fbece14.

--- src/main/java/pku/AbstractDomain.java
+++ src/main/java/pku/AbstractDomain.java
@@ -10,21 +10,44 @@ public class AbstractDomain {
     private HashMap<Integer, BitSet> domain;
     private HashMap<String, Integer> name2index;
     private HashMap<Integer, String> index2name;
+    private HashMap<Integer, String> nameType;
+    private HashMap<Integer, Integer> malloc2index;
+    private HashMap<Integer, Integer> index2malloc;
+    private HashMap<Integer, String> mallocType;
 
     public AbstractDomain() {
         domain = new HashMap<>();
         name2index = new HashMap<>();
         index2name = new HashMap<>();
+        malloc2index = new HashMap<>();
+        index2malloc = new HashMap<>();
+        nameType = new HashMap<>();
+        mallocType = new HashMap<>();
+    }
+
+    // add a new malloc to the domain
+    public Integer addMalloc(int malloc, String type) {
+        if (!malloc2index.containsKey(malloc)) {
+            int index = malloc2index.size();
+            malloc2index.put(malloc, index);
+            index2malloc.put(index, malloc);
+            mallocType.put(index, type);
+            return index;
+        }
+        return -1;
     }
 
     // add a new element to the domain
-    public void add(String name) {
+    public Integer addName(String name, String type) {
         if (!name2index.containsKey(name)) {
             int index = name2index.size();
             name2index.put(name, index);
             index2name.put(index, name);
+            nameType.put(index, type);
             domain.put(index, new BitSet());
+            return index;
         }
+        return -1;
     }
 
     // set the value of an element in the domain
@@ -32,8 +55,22 @@ public class AbstractDomain {
         domain.put(index, value);
     }
 
+    public void set(String name, BitSet value) {
+        int index = name2index.get(name);
+        domain.put(index, value);
+    }
+
+    public void setType(int index, String type) {
+        nameType.put(index, type);
+    }
+
     // get the value of an element in the domain
     public BitSet get(int index) {
+        return domain.get(index);
+    }
+
+    public BitSet get(String name) {
+        int index = name2index.get(name);
         return domain.get(index);
     }
 
