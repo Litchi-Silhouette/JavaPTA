36a98740cbdde98af4b30102f5e99b627c3938e6
zhi102 <lizhiyi@stu.pku.edu.cn>
2024-11-29 22:25:53 +0800

    set up the workflow for cs interprocedural analysis

--- src/main/java/pku/PointerAnalysis.java
+++ src/main/java/pku/PointerAnalysis.java
@@ -6,6 +6,13 @@ import pascal.taie.World;
 import pascal.taie.analysis.ProgramAnalysis;
 import pascal.taie.config.AnalysisConfig;
 import pku.abs.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import pku.Context;
+import pascal.taie.ir.IR;
+import pascal.taie.ir.stmt.Invoke;
+import pascal.taie.language.classes.JMethod;
 
 public class PointerAnalysis extends PointerAnalysisTrivial {
     public static final String ID = "pku-pta";
@@ -16,12 +23,16 @@ public class PointerAnalysis extends PointerAnalysisTrivial {
 
     @Override
     public PointerAnalysisResult analyze() {
-        var result = new PointerAnalysisResult();
+        var PTAresult = new PointerAnalysisResult();
         var preprocess = new PreprocessResult();
+        var result = new InterproceduralConstraintResult();
         var domain = new AbstractVarDomain();
         var world = World.get();
         var main = world.getMainMethod();
         var jclass = main.getDeclaringClass();
+        List<Context> workList = new ArrayList<>();
+        List<Integer> visited = new ArrayList<>();
+        
 
         // TODO
         // You need to use `preprocess` like in PointerAnalysisTrivial
@@ -39,13 +50,41 @@ public class PointerAnalysis extends PointerAnalysisTrivial {
             });
 
             tjclass.getDeclaredMethods().forEach(method -> {
-                if (!method.isAbstract())
-                    preprocess.analysis(method.getIR());
+                if (method.isAbstract())
+                    return;
+                Context context = new Context(null, method.getIR());
+                int currentContextId = context.hashCode();
+
+                // domain and preprocess continually updated
+                var mcr = new MethodConstraintResult(preprocess, domain);
+                System.out.println("Context: " + context.getName() + " " + currentContextId);
+                mcr.analysis(context);
+
+                // add all invoke contexts to worklist and add interprocedual constraints
+                result.addInterprocedualConstraint(mcr, currentContextId, workList);
             });
         });
+        
+        // BFS, deal with 1-level context
+        while (!workList.isEmpty()) {
+            Context context = workList.remove(0);
+            int currentContextId = context.hashCode();
+            if (visited.contains(currentContextId)) {
+                continue;
+            } else {
+                visited.add(currentContextId);
+            }
+
+            // domain and preprocess continually updated
+            var mcr = new MethodConstraintResult(preprocess, domain);
+            System.out.println("Context: " + context.getName() + " " + currentContextId);
+            mcr.analysis(context);
+
+            // add all invoke contexts to worklist and add interprocedual constraints
+            result.addInterprocedualConstraint(mcr, currentContextId, workList);
+        }
 
         return super.analyze();
         // return result;
     }
-
 }
