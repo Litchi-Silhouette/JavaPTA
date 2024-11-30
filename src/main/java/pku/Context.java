b4a95b1a8c4c252d71ec03ce10fa266932e064d8
zhi102 <lizhiyi@stu.pku.edu.cn>
2024-11-30 17:29:24 +0800

    modify comment

--- src/main/java/pku/Context.java
+++ src/main/java/pku/Context.java
@@ -4,20 +4,20 @@ import pascal.taie.ir.IR;
 import pascal.taie.ir.stmt.*;
 
 public class Context {
-    // private final JMethod method;
-    /*
-     * @param lineNumber: the line number of the statement invoking this context in the source code, -1 if not invoked
-     * @param index: the index of the statement invoking this context in the caller, -1 if not invoked
-     * @param name: the name of the context
-     * @param bodyIR: the body of this method
-     */
+    
     private final int lineNumber;
     private final int index;
     private final String methodName;
     private final String className;
     private final String name;
     private final IR bodyIR;
-
+    
+    /**
+     * Constructor of Context
+     * @param stmt: the statement that this context is invoked, null if it is not invoked
+     * @param ir: IR of the callee method
+     * 
+     */
     public Context(Stmt stmt, IR ir) {
         if (stmt == null) {
             this.lineNumber = -1;
